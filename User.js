395aff853ecfd627cf2a4c798ab29d17f890ce65
Saumy0010 <s8795p@gmail.com>
2022-07-31 16:26:01 +0530

    Navbar changes

--- User.js
+++ User.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     name: String,
-    age: Number
+    age: {
+        type: Number,
+        min: 18,
+        max: 100,
+    }
 });
 
 module.exports = mongoose.model('User', userSchema);
